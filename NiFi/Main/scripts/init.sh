326a8c2fc76404357ae62aadb2e7beeb74fb1be5
Martijn Dekkers <44741536+MartijnDekkers@users.noreply.github.com>
2020-04-19 11:51:36 +0200

    Clean up main image, we are mutable configuration out of Consul now

---
+++ NiFi/Main/scripts/init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Get the ContainerPilot configuration
+/usr/local/bin/consul kv get ${SCRIPT_SRC_PATH}/containerpilot.json5 > ${SCRIPTS_DIR}/containerpilot.json5
+
+# run ContainerPilot
+exec /usr/local/bin/containerpilot -config /scripts/containerpilot.json5
